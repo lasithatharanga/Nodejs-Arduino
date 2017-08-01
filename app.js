263076135f74d2f5904152de420ce4db53d23c07
lasithatharanga <lasitha119@gmail.com>
2017-08-01 17:48:52 +0530

    error correction

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-var serialport=require("serialport");
-var SerialPort =serialport.SerialPort;
-var portName = process.argv[2];
-
-var myPort = new SerialPort(portName,{
-  baudRate:9600,
-  parser:serialport.parsers.readline("\r\n")
-})
-
+var SerialPort = require('serialport');
+var myPort = new SerialPort('COM4', function (err) {
+  if (err) {
+    return console.log('Error: ', err.message);
+  }
+});
+// var serialport=require("serialport");
+// var SerialPort =serialport.SerialPort;
+// var portName = process.argv[2];
+ console.log(SerialPort);
+// var myPort = new SerialPort(portName);
+//
 myPort.on('open',onOpen);
 myPort.on('data',onData);
-
-function onOpen(){
+//
+ function onOpen(){
   console.log("Open Connection");
-}
+ }
 
 function onData(data){
   console.log(data);
